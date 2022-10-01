61e7cdb156756aa8c1995c0cee3103bbefb846b8
Alec Sherlock <asher@Alecs-MacBook-Pro-2.local>
2022-09-30 19:01:48 -0600

    setting up Spring REST

---
+++ backend/spring-boot-ecomm/src/main/java/com/alectraining/ecomm/SpringBootEcommApplication.java
@@ -0,0 +1,13 @@
+package com.alectraining.ecomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEcommApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEcommApplication.class, args);
+	}
+
+}
---
+++ backend/spring-boot-ecomm/src/test/java/com/alectraining/ecomm/SpringBootEcommApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alectraining.ecomm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEcommApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
