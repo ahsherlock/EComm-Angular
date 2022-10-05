f9b395a6226476d089fb8e602ad30305e9cf2b74
Alec Sherlock <asher@Alecs-MacBook-Pro-2.local>
2022-10-04 18:21:25 -0600

    dynamically added product categories, search functionality achieved, product master view added

--- frontend/angular-ecomm/src/app/services/product.service.ts
+++ frontend/angular-ecomm/src/app/services/product.service.ts
@@ -3,24 +3,55 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from '../common/product';
 import { map } from 'rxjs/operators';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+
+
+
   private baseUrl = 'http://localhost:8080/api/products';
+  private categoryUrl = 'http://localhost:8080/api/product-category';
+
   constructor(private httpClient:HttpClient) { }
 
   getProductList(theCategoryId:number):Observable<Product[]>{
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
-    
-    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+    return this.getProducts(searchUrl);
+  }
+  searchProducts(keyWord: string): Observable<Product[]> {
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${keyWord}`;
+    return this.getProducts(searchUrl);
+  }
+
+  private getProducts(searchUrl: string): Observable<Product[]> {
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
-      );
+    );
+  }
+
+  getProduct(productId: number): Observable<Product> {
+    const productUrl = `${this.baseUrl}/${productId}`;
+    return this.httpClient.get<Product>(productUrl);
+  }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
+    );
   }
 }
-interface GetResponse{
+interface GetResponseProducts{
   _embedded:{
     products:Product[];
   }
+
+}
+interface GetResponseProductCategory{
+  _embedded:{
+    productCategory:ProductCategory[];
+  }
+
 }
