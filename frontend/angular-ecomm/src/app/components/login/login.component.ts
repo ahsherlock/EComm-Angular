2bfa7f56ae3c42657ac24221d9f9750ae7857d30
Alec Sherlock <asher@Alecs-MacBook-Pro-2.local>
2022-10-13 19:52:04 -0600

    added VIP member access functionality

--- frontend/angular-ecomm/src/app/components/login/login.component.ts
+++ frontend/angular-ecomm/src/app/components/login/login.component.ts
@@ -13,7 +13,7 @@ export class LoginComponent implements OnInit {
 
   constructor(@Inject(OKTA_AUTH)private oktaAuth: OktaAuth) {
     this.oktaSignin = new OktaSignIn({
-      logo:'assets/images/logo.png',
+      logo:'assets/images/logo.jpeg',
       baseUrl: myAppConfig.oidc.issuer.split('/oauth2')[0],
       clientId: myAppConfig.oidc.clientId,
       redirectUri: myAppConfig.oidc.redirectUri,
